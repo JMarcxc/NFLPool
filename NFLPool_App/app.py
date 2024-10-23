59823792101b033b27eabe4a9e3fc6457c829d55
Jon <jon.marcus@emory.edu>
2024-10-22 22:00:56 -0400

    default template

--- NFLPool_App/app.py
+++ NFLPool_App/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to Jon\'s NFL Pool!'
+    return render_template('index.html')  # Render the index.html file
 
 if __name__ == '__main__':
     app.run(debug=True)
